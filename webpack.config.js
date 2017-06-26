39969cde82e40166ce246d71eac103b517510865
johnliu <john.liu@karmadata.com>
2017-06-26 17:35:38 -0400

    added babel and uglify to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
+let webpack = require("webpack")
+
 module.exports = {
   entry: "./browser.js",
   output: {
     filename: "dist/thorz.js"
-  }
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
 }
